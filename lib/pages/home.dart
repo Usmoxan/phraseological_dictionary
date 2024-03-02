a469bfebb7b6fc3ec2ade9c42ff1be19acf0c0c7
Usmoxan Tadjibayev <101461756+Usmoxan@users.noreply.github.com>
2024-03-02 11:51:49 +0500

    f

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:phraseological_dictionary/pages/about.dart';
 import 'package:phraseological_dictionary/pages/ph_unit.dart';
@@ -28,38 +29,85 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         title: const Text("Phraseological dictionary"),
       ),
-      body: IndexedStack(
-        index: selectedPageIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedLabelStyle: const TextStyle(
-          fontSize: 12,
-        ),
-        backgroundColor: Theme.of(context).brightness == Brightness.light
-            ? Colors.white
-            : Colors.black,
-        currentIndex: selectedPageIndex,
-        onTap: (int index) {
-          setState(() {
-            selectedPageIndex = index;
-          });
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard_rounded),
-            label: 'PHRASEOLOGICAL UNITS',
+      body: Column(
+        children: [
+          Expanded(
+            child: IndexedStack(
+              index: selectedPageIndex,
+              children: _pages,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.space_dashboard),
-            label: 'PROVERBS AND SAYINGS',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.help),
-            label: 'Haqida',
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(5),
+            child: const Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  children: [
+                    Icon(Icons.dashboard_rounded),
+                    Text(
+                      'PHRASEOLOGICAL UNITS',
+                      style: TextStyle(fontSize: 12),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                )),
+                Expanded(
+                    child: Column(
+                      children: [
+                        Icon(Icons.space_dashboard),
+                        Text(
+                          'PROVERBS AND SAYINGS',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12),
+                        )
+                      ],
+                    )),
+                Expanded(
+                    child: Column(
+                  children: [
+                    Icon(Icons.help),
+                    Text(
+                      'Haqida',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ],
+                )),
+              ],
+            ),
           ),
         ],
       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   selectedLabelStyle: const TextStyle(
+      //     fontSize: 12,
+      //   ),
+      //   backgroundColor: Theme.of(context).brightness == Brightness.light
+      //       ? Colors.white
+      //       : Colors.black,
+      //   currentIndex: selectedPageIndex,
+      //   onTap: (int index) {
+      //     setState(() {
+      //       selectedPageIndex = index;
+      //     });
+      //   },
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.dashboard_rounded),
+      //       label: 'PHRASEOLOGICAL UNITS',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.space_dashboard),
+      //       label: 'PROVERBS AND SAYINGS',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.help),
+      //       label: 'Haqida',
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
